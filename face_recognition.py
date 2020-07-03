c7dce9f801d8247477968b34f5acf2eda015c8c1
Riya Patel <riaupatel13@yahoo.com>
2020-07-03 11:11:18 -0400

    if verifier is wrong, user can input correct name and database will update accordingly. deletes pkl file at end of verification

--- face_recognition.py
+++ face_recognition.py
@@ -1,18 +1,20 @@
 from deepface import DeepFace
 import pandas as pd
 import os
+import glob
 
 '''
 TODO:
-- delete pkl file after running everytime (will be slow to create representation but thats ok)
-- If no, provide a list of all options and ask who and update - wrong_answer
 - (another file/later) take picture and save it
 - (another file/later) real-time analysis + take pic + save 
 - works with os, have it work with cloud 
+- error check for if the image cannot be found (generally shouldn't happen)
 '''
 '''
 QUESTIONS:
 - what to do when database gets too large? delete earlier pictures? set a threshold
+- deleted pkl file after every verification bc we are updating database, but it runs 3x slower
+    - 11s for 5 pictures --> doesn't scale well
 '''
 
 
@@ -33,40 +35,65 @@ database_path = "faces" # add this to setup file
 img = "test.jpg" # somehow make this so that its not in the file
 
 
-def extract_name(path):
+def name_from_pic(path):
     dir_name = path.split("/")[1]
     name = dir_name.replace("_", " ")
     return name
 
-def update_database(pic, sim_img_path):
-    dir_name = sim_img_path.split("/")
-    dir_list = os.listdir(dir_name[0]+'/'+dir_name[1])
+def update_database(pic, db_path, person):
+    dir_list = os.listdir(db_path+'/'+person)
     file_count = len(dir_list)
-    os.rename(pic,dir_name[0]+'/'+dir_name[1]+'/'+dir_name[1]+'_'+str(file_count)+'.jpg')
+    os.rename(pic,db_path+'/'+person+'/'+person+'_'+str(file_count)+'.jpg')
+    # copy instead of just move because you lose the file (potentially an issue)
+    print(f"{person} has been signed in")  
 
+def list_names(db_path):
+    people_dir = os.listdir(db_path)
+    # create list of people
+    i = 0
+    while i < len(people_dir):
+        # do not include .pkl file or .DS_Store file
+        # eliminate all non name files
+        if "." in people_dir[i]:
+            people_dir.remove(people_dir[i])
+        # extracting name
+        else:
+            people_dir[i] = people_dir[i].replace("_", " ")
+            i += 1
 
+    return people_dir
+
+def wrong_answer(pic, db_path):
+    name_list = list_names(db_path)
+    person = None
+    while person not in name_list:
+        person = input(f"Type in the correct person. {name_list} \n")
+    update_database(pic, db_path, person)
+    
 '''
-Creates pkl file with representation of database
-Delete pkl file when updating database
+Creates pkl file with representation of database and deletes at end
 Optionally can choose model and metrics (but may need to load model)
 '''
-df = DeepFace.find(img_path = img, db_path = "faces")
+df = DeepFace.find(img_path = img, db_path = database_path)
 
 if df.shape[0] > 0:
     most_sim_pic = df.iloc[0].identity
-    person = extract_name(most_sim_pic) # extrace name from file path
+    person = name_from_pic(most_sim_pic) # extrace name from file path
     
     verify = None
     while verify != 'Y' and verify != 'N':
         verify = input(f"Is the person {person}? Please select Y/N.\n")
     
     if verify == 'Y':
-        update_database(img, most_sim_pic)
-        print(f"{person} has been signed in")  
+        update_database(img, database_path, person)
     elif verify == 'N':
-        # wrong_answer() # update_database() called in function
-        print("oops")
+        wrong_answer(img, database_path) # update_database() called in function
 
 else:
     print("Unable to identify person")
-    # wrong_answer()
+    wrong_answer(img, database_path)
+
+# remove pkl file from database because it has been updated
+pkl_files = glob.glob(database_path+'/'+ '*.pkl')
+for file in pkl_files:
+    os.remove(file) 
